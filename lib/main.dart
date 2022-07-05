187cc9bdd031dfc396fd5e9c8882a182df1af12b
phatlam1811@gmail.com <phatlam1811@gmail.com>
2022-07-05 18:03:54 +0700

    Initialized Login Layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,54 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
-  home: Scaffold(
-    appBar: AppBar(
-      title: const Text('My Appbar!'),
-      centerTitle: true,
-    ),
-    body: const Center(
-      child: Text("Application Body!"),
-    ),
-    floatingActionButton: const FloatingActionButton(
-      onPressed: null,
-      child: Text('Click'),
-    ),
-  )
-));
+void main() {
+  runApp(const MaterialApp(
+    home: LoginWidget(),
+  ));
+}
+
+class LoginWidget extends StatelessWidget {
+  const LoginWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const Text(
+              'C@nnect',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 30.0,
+              ),
+            ),
+
+            const SizedBox( height: 70.0 ),
+
+            const Text(
+              'Email or Phone Number',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 16.0
+              ),
+            ),
+
+            const SizedBox( height: 10.0),
+
+            const SizedBox( height: 30.0 ),
+
+            const Text(
+              'Password',
+              style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 16.0
+              ),
+            ),
+
+            Row(),
+          ],
+      )
+    );
+  }
+}
